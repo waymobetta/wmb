ad40fc63579c3d7cf6ac77fea13ad965c77e9e3b
waymobetta <jbroethke@gmail.com>
2018-01-15 22:42:42 -0800

    add Slack method

--- wmb.go
+++ wmb.go
@@ -3,12 +3,22 @@
 package wmb
 
 import (
+	"log"
 	"os"
 	"os/exec"
+
+	"github.com/waymobetta/goslack"
 )
 
 func Clear() {
 	c := exec.Command("clear")
 	c.Stdout = os.Stdout
 	c.Run()
+}
+
+func Slack(payload string) {
+	_, err := goslack.Send(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
 }

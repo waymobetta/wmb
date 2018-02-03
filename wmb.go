8d7dd54cbfdaa85a0a038e3a4dacee6fb5e86e61
waymobetta <jbroethke@gmail.com>
2018-02-03 01:13:39 -0800

    add WriteFile + ReadFileByLine methods

--- wmb.go
+++ wmb.go
@@ -3,7 +3,9 @@
 package wmb
 
 import (
+	"bufio"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -19,7 +21,7 @@ func Clear() {
 func JSONToString(fileName string) (string, error) {
 	raw, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		return "", err
+		log.Fatal("[!] Error encountered when reading JSON file\n", err)
 	}
 	return string(raw), nil
 }
@@ -27,7 +29,45 @@ func JSONToString(fileName string) (string, error) {
 // TermNotify notifies terminal with data
 func TermNotify(data string) error {
 	if err := exec.Command("terminal-notifier", "-message", data).Run(); err != nil {
-		return err
+		log.Fatal("[!] Error encountered when attemping to notify user via TermNotify\n", err)
+	}
+	return nil
+}
+
+// ReadFileByLine reads a file line-by-line
+func ReadFileByLine(path string) ([]string, int, error) {
+	data := []string{}
+
+	file, err := os.Open(path)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("[!] Error encountered when opening file\n", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+	return data, len(data), nil
+}
+
+// WriteFile writes strings to a file, delimited in some fashion prior to being passed in as an argument
+func WriteFile(path string, data string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal("[!] Error encountered when creating file\n", err)
+	}
+	defer file.Close()
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("[!] Error encountered when opening file\n", err)
+	}
+	defer f.Close()
+	if _, err = f.WriteString(data); err != nil {
+		log.Fatal("[!] Error encountered when writing to file\n", err)
 	}
 	return nil
 }
